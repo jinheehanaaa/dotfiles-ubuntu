0e9a5a5683cd75e4b50dee3b6af5241a2d2083c7
Jinhee Han <80008300+jinheehanaaa@users.noreply.github.com>
2023-02-01 19:50:34 -0600

    Symlink sh to zsh & Prevent superfluoous commands with conditionals

--- setup_zsh.zsh
+++ setup_zsh.zsh
@@ -2,10 +2,29 @@
 
 echo "\n<<< Starting ZSH Setup >>>\n"
 
-# Installation unnecessary; i9t's in the Brewfile
+# Installation unnecessary; it's in the Brewfile.
 
-echo "Enter superuser (sudo) password to edit /etc/shells"
-echo '/home/linuxbrew/.linuxbrew/bin/zsh' | sudo tee -a '/etc/shells' >/dev/null
+# https://stackoverflow.com/a/4749368/1341838
 
-echo "Enter user password to change login shell"
-chsh -s '/home/linuxbrew/.linuxbrew/bin/zsh'
+# 1. See if new SHELL PATH exists in etc
+if grep -Fxq '/home/linuxbrew/.linuxbrew/bin/zsh' '/etc/shells'; then
+    echo '/home/linuxbrew/.linuxbrew/bin/zsh already exists in /etc/shells'
+else
+echo '/usr/bin/zsh' | sudo tee -a '/etc/shells' >/dev/null
+fi 
+
+# 2. Look for env var for default SHELL to match new path
+if [ "$SHELL" = '/home/linuxbrew/.linuxbrew/bin/zsh' ]; then
+    echo '$SHELL is already /usr/bin/zsh'
+else
+    echo "Enter user password to change login shell"
+    chsh -s '/usr/bin/zsh'
+fi
+
+# 3. Use version option 
+if sh --version | grep -q zsh; then 
+    echo '/usr/bin/sh already linked to /bin/zsh'
+else
+    echo "Enter superuser (sudo) password to symlink sh to zsh"
+    sudo ln -sfv /home/linuxbrew/.linuxbrew/bin/zsh /usr/bin/sh
+fi
--- shebang.zsh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env zsh
-
-echo "\n<<< Running $0 >>>\n"
-echo "The current Shell is: $(ps $$ -ocomm=)"
-
-exists brew && echo "Hey!" || echo "bogus"
-exists nonexistent && echo "Hey!" || echo "bogus"
