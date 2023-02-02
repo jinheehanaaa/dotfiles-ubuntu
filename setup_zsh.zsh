f90aa591286d4de73ef2d4fa62fc1d595c850f4b
Jinhee Han <80008300+jinheehanaaa@users.noreply.github.com>
2023-02-01 19:19:04 -0600

    Redirect standard output to the blackhole

--- setup_zsh.zsh
+++ setup_zsh.zsh
@@ -5,7 +5,7 @@ echo "\n<<< Starting ZSH Setup >>>\n"
 # Installation unnecessary; i9t's in the Brewfile
 
 echo "Enter superuser (sudo) password to edit /etc/shells"
-echo '/home/linuxbrew/.linuxbrew/bin/zsh' | sudo tee -a '/etc/shells'
+echo '/home/linuxbrew/.linuxbrew/bin/zsh' | sudo tee -a '/etc/shells' >/dev/null
 
 echo "Enter user password to change login shell"
 chsh -s '/home/linuxbrew/.linuxbrew/bin/zsh'
